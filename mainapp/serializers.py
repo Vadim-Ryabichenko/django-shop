34823cf8d8018c4b028cddfc293f7d42983f90d9
Vadim <vadimrembyboy@gmail.com>
2024-01-02 02:17:30 +0200

    Serializers homework update

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -10,7 +10,7 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = ['id', 'name', 'text', 'price', 'count_in_storage']
 
-
+'''
 class ClientSerializer(serializers.ModelSerializer):
     
     user = serializers.PrimaryKeyRelatedField(read_only = True)
@@ -18,24 +18,54 @@ class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ['user', 'wallet']
-
 '''
+'''
+class PurchaseSerializer(serializers.ModelSerializer): # для отримання Покупки за даними Клієнта
+
+    user = ClientSerializer()
+    product = ProductSerializer(many=True)
+    
+    class Meta:
+        model = Purchase
+        fields = ['id', 'user', 'product', 'count']
+'''
+
+
 class PurchaseSerializer(serializers.ModelSerializer):    
 
     product = ProductSerializer(read_only = True, many=True)
+    
+    class Meta:
+        model = Purchase
+        fields = ['id', 'user', 'product', 'count']
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    purchase = serializers.StringRelatedField(read_only = True, many=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'purchase']
+
+
+class ClientSerializer(serializers.ModelSerializer): # для отримання Клієнта з покупками
+
+    purchase_set = serializers.SerializerMethodField()
+
+    def get_purchase_set(self, obj):
+        purchases = obj.purchase_set.all()
+        serialized_purchases = []
+        for purchase in purchases:
+            serialized_purchases.append({
+                'id': purchase.id,
+                'purchase': purchase,
+                'count': purchase.count
+            })
+        return serialized_purchases
+    
     user = serializers.PrimaryKeyRelatedField(read_only = True)
-    
+
     class Meta:
-        model = Purchase
-        fields = ['id', 'user', 'product', 'count']
-'''
-
-
-class PurchaseSerializer(serializers.ModelSerializer):  # це для завдання 3
-
-    user = ClientSerializer()
-    
-    class Meta:
-        model = Purchase
-        fields = ['id', 'user', 'product', 'count']
-
+        model = Client
+        fields = ['user', 'wallet', 'purchase_set']
